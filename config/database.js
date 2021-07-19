7deb0239d8e5fd4ec4b0723d978213b4d8606f3e
andres manuel hense <ahense@dc.uba.ar>
2021-07-19 17:09:02 -0300

    security fix

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
+const {database,user,password,host}=require('./keys.js');
 const {Sequelize}=require('sequelize');
 
-module.exports=new Sequelize('d6l91hsp1ur52b','ajscbdcwhgzuyq','f310a7210326ee9c97aaee994b072e4e2f1ab870dcbb6714b89f6e464a9678b3',{
-	host: 'ec2-52-202-152-4.compute-1.amazonaws.com',
+module.exports=new Sequelize(database,user,password,{
+	host:host,
 	dialect:'postgres',
 	protocol:'postgres',
 	dialectOptions:{
